b895e021fa9798919038b9cde9777918a10db187
Nimira43 <n.rabone72@aol.com>
2026-09-12 00:05:28 +0100

    Added  simplex noise, random number generator and seeder. Also added grass and dirt blocks.

---
+++ src/scripts/blocks.js
@@ -0,0 +1,16 @@
+export const blocks = {
+  empty: {
+    id: 0,
+    name: 'empty'
+  },
+  grass: {
+    id: 1,
+    name: 'grass',
+    colour: 0x559020
+  },
+  dirt: {
+    id: 2,
+    name: 'dirt',
+    colour: 0x807020
+  }
+}
